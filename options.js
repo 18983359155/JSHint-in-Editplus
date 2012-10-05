000ef90988326b7510d724e71b8815a435f07794
cuixiping <cuixiping@gmail.com>
2012-10-06 04:17:00 +0800

    add options.js

---
+++ options.js
@@ -0,0 +1,15 @@
+options = {
+	forin: true,
+	noarg: true,
+	noempty: false,
+	curly: true,
+	browser: true,
+	jquery: true,
+	indent: 1,
+	maxerr: 50,
+	laxbreak: true,
+	expr: true,
+	funcscope : true,
+	sub : true,
+	white: false
+}
